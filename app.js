30b44d7d30297492c02408210a3ee6cc58566db8
Illusion1 <nguyencuong.20082009@gmail.com>
2025-05-05 02:34:11 +0700

    Increase JSON and URL-encoded body size limits to 10mb in app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var blogRouter = require('./routes/blog')
 var chatRouter = require('./routes/chat')
 
 var app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 const cors = require('cors');
 require('dotenv').config();
 
@@ -49,8 +49,8 @@ hbs.registerHelper('block', function(name) {
 
 app.use(logger('dev'));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
